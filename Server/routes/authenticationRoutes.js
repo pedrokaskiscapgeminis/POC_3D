1b533e3768fd4ed6b97c73e0ba8f3980f5fcd103
Josema14 <josemaespin14@gmail.com>
2022-12-07 13:59:27 +0100

    Encriptado
    Contraseñas encriptadas y eliminadas funciones deprecated

--- Server/routes/authenticationRoutes.js
+++ Server/routes/authenticationRoutes.js
@@ -1,28 +1,42 @@
 const mongoose = require("mongoose");
 
+//bcrypt
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+
+
 const User = require("../model/User");
 module.exports = (app) => {
   //Routes
   app.post("/users", async (req, res) => {
-    console.log(req.body);
+    
     const { username, password } = req.body;
     if (username == "" || password == "") {
-      res.send(400,"Invalid credentials");
+      res.status(400).send("Invalid credentials");
       return;
     }
-
+  
     let userAccount = await User.findOne({ username: username });
     if (userAccount != null) {
-      if (password == userAccount.password) {
+       
+      if (await bcrypt.compare(password,userAccount.password)) {
         userAccount.lastAuthentication = Date.now();
         await userAccount.save();
 
-        res.send(200,userAccount);
+        res.status(200).send({
+
+          id: userAccount._id,
+          username: userAccount.username,
+          lastAuthentication: userAccount.lastAuthentication,
+
+        
+        });
         return;
       }
     }
 
-    res.send(400,"Invalid credentials");
+    res.status(400).send("Invalid credentials");
     return;
   });
 
@@ -30,7 +44,7 @@ module.exports = (app) => {
     console.log(req.body);
     const { username, password } = req.body;
     if (username == "" || password == "") {
-      res.send(400,"Invalid credentials");
+      res.status(400).send("Invalid credentials");
       return;
     }
 
@@ -38,19 +52,30 @@ module.exports = (app) => {
     if (userAccount == null) {
       console.log("Create new account");
 
+      //Hashing password
+
+      let hashPassword = await bcrypt.hash(password, saltRounds);
+
       let newUser = new User({
         username: username,
-        password: password,
+        password: hashPassword,
 
         lastAuthentication: Date.now(),
       });
 
       await newUser.save();
 
-      res.send(200,newUser);
+      res.status(201).send({
+
+        id: newUser._id,
+        username: newUser.username,
+        lastAuthentication: newUser.lastAuthentication,
+
+      
+      });
       return;
     } else {
-      res.send(400,"Username is already taken");
+      res.status(400).send("Username is already taken");
       
     }
     return
