badca31ff19693dc324800759c94b50bc014c4c9
pedrokaskiscapgeminis <107995959+pedrokaskiscapgeminis@users.noreply.github.com>
2022-12-12 10:22:06 +0100

    Lobby and choose
    Lobby and choose

---
+++ Client/Assets/Script/PlayerSpawn.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerSpawn : MonoBehaviour
+{
+public GameObject[] playerPrefabs;
+public Transform[] spawnPoints;
+
+private void Start() {
+    {
+        int randomNumber = Random.Range(0, spawnPoints.Length);
+        Transform spawnPoint = spawnPoints[randomNumber];
+        GameObject playerToSpawn = playerPrefabs[(int)PhotonNetwork.LocalPlayer.CustomProperties["playerAvatar"]];
+        PhotonNetwork.Instantiate(playerToSpawn.name, spawnPoint.position, Quaternion.identity);
+    }
+}
+}
