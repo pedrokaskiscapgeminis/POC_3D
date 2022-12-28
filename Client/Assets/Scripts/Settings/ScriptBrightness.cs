039e64e6b2be7f81031186f84ccdff4782b67201
Josema14 <josemaespin14@gmail.com>
2022-12-28 10:12:27 +0100

    Ajustes Brillo y Resoluciones con hercios
    Ajustes de brillo y resoluciones con hercios

--- Client/Assets/Scripts/Settings/ScriptBrightness.cs
+++ Client/Assets/Scripts/Settings/ScriptBrightness.cs
@@ -2,30 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
 
 public class ScriptBrightness : MonoBehaviour
 {
    public Slider slider;
-    public float sliderValue;
-     public float GammaCorrection;
+
+   public PostProcessProfile brightness;
+   public PostProcessLayer layer;
+
+   AutoExposure exposure;
   
 
     // Start is called before the first frame update
     void Start()
     {
-        RenderSettings.ambientLight = new Color(GammaCorrection, GammaCorrection, GammaCorrection, 0.5f);
+        brightness.TryGetSettings(out exposure);
+        exposure.keyValue.value = 0.75f;
+   
     }
 
     //Update is called once per frame
-    void Update()
+   
+    public void AdjustBrightness(float value)
     {
 
-    }
-
-    public void ChangeSlider(float value)
-    {
-
-        GammaCorrection = value;
+       if(value != 0)
+       {
+        exposure.keyValue.value = value;
+       }
+      
        
     }
 }
